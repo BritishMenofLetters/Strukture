5fd0de6dc732204d1da7f5d745bccc169e1f8ff0
BritishMenofLetters <73581730+BritishMenofLetters@users.noreply.github.com>
2020-10-28 10:07:45 +0100

    Add files via upload

---
+++ Datoteke.c
@@ -0,0 +1,87 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct student stu;
+
+struct student{
+	char ime[30];
+	char prezime[30];
+	int	brBodova;
+};
+
+
+void main() {
+	
+	FILE* fp = NULL;
+	int numBytes=0, i = 0, brStu = 0;
+
+	fp = fopen("studenti.txt", "r");
+	if (fp == NULL)	return -1;
+
+	fseek(fp, 0, SEEK_END);
+	numBytes = ftell(fp);
+
+	fseek(fp, 0, SEEK_SET);
+
+	char* buffer = (char*)calloc(numBytes, sizeof(char));
+	if (buffer == NULL)	return 1;
+
+	fread(buffer, sizeof(char), numBytes, fp);
+	fclose(fp);
+
+	while (1) {
+		if (buffer[i] == '\0')	break;
+		
+		if (buffer[i++] != '\n' && buffer[i] == '\n')
+			brStu++;
+	}
+
+	stu* nizStu = (stu*)malloc(sizeof(stu)*brStu);
+	if (nizStu == NULL) return -1;
+	
+	char* prebacivac = (char*)malloc(sizeof(char));
+	if (prebacivac == NULL) return -1;
+
+	prebacivac = &buffer[0];
+	i = 0;
+	while (i < brStu){
+		strcpy(nizStu[i].ime,"\0");
+		strcpy(nizStu[i].prezime,"\0");
+
+			while (prebacivac[0] != ' ' && prebacivac[0] != '\n')
+				strncat( nizStu[i].ime,prebacivac++,1);
+			prebacivac++;
+
+			while (prebacivac[0] != ' ' && prebacivac[0] != '\n')
+				strncat( nizStu[i].prezime,prebacivac++,1);
+			prebacivac++;
+
+			char* a = (char*)malloc(4 * sizeof(char));
+			if (a == NULL)	return -1;
+
+			*a = '\0';
+			while (prebacivac[0] != ' ' && prebacivac[0] != '\n') {
+				strncat(a, prebacivac++,1);
+			}
+			nizStu[i].brBodova = atoi(a);
+			free(a);
+			if (++i == brStu)	break;
+			prebacivac++;
+			
+	}
+	
+	//free(prebacivac);
+	free(buffer);
+
+	int maxBod = 120;
+	printf("Ime\tPrezime\tAp.Bod\tRe.Bod\n");
+	for (i = 0; i < brStu; i++)
+		printf("%s\t%s\t%d\t%.2f\n", nizStu[i].ime, nizStu[i].prezime, nizStu[i].brBodova,nizStu[i].brBodova/(float)maxBod*100);
+
+	free(nizStu);
+
+	return 0;
+}
---
+++ Liste.c
@@ -0,0 +1,137 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct osoba list;
+typedef list* position;
+
+struct osoba{
+	char ime[30];
+	char prezime[30];
+	int godRodenja;
+
+	position next;
+};
+
+//Upisuje elemente strukture
+void EnterElements(position q) {
+	
+	rewind(stdin);
+	printf("Unesite ime: ");
+	fgets(q->ime, 10, stdin);
+
+	rewind(stdin);
+	printf("Unesite prezime: ");
+	fgets(q->prezime, 20, stdin);
+
+	rewind(stdin);
+	printf("Unesite godinu rodenja: ");
+	scanf("%d", &q->godRodenja);
+}
+
+//trazi poziciju zadnjeg cvora liste
+position FindLast(position headNext) {
+	while (headNext->next != NULL)
+		headNext = headNext->next;
+	return headNext;
+}
+
+//d) pronalazi element u listi
+position FindElement(position headNext, char elem[]) {
+	if (headNext == NULL)	printf("Takav element ne postoji!");
+
+	while (headNext != NULL && strcmp(headNext->prezime, elem))
+		headNext = headNext->next;
+
+	return headNext;
+}
+
+//trazi prijasnji cvor cvora koji sadrzi elem[] (Prezime)
+position FindPrev(position head, char elem[]) {
+	position prev = NULL, curr = NULL;
+	prev = head;
+	curr = prev->next;
+
+	while (curr != NULL && strcmp(curr->prezime, elem)) {
+		prev = curr;
+		curr = prev->next;
+	}
+	
+	if (curr == NULL)	return NULL;
+
+	return prev;
+}
+
+//a) dinamièki dodaje novi element na poèetak liste
+int addAtStart(position start) {
+	position q = (position)malloc(sizeof(list));
+	if (q == NULL) {
+		printf("Greska prilikom alociranja memorije\n");  
+		return -1;
+	}
+	
+	EnterElements(q);
+
+	q->next = start->next;
+	start->next = q;
+
+	return 0;
+}
+
+//b) ispisuje listu
+int printList(position head) {
+	if (head == NULL) {
+		printf("Nema elemenata. Lista je prazna");
+		return -1;
+	}
+	
+	while (head != NULL) {
+		printf("%s\t%s\t%d\n", head->ime, head->prezime, head->godRodenja);
+		head = head->next;
+	}
+	return 0;
+}
+
+//c) dinamièki dodaje novi element na kraj liste
+int addAtEnd(position head) {
+	position last = NULL;
+	last = FindLast(head->next);
+	
+	if (last == NULL) {
+		printf("Greska! Nije pronaden zadnji element liste.");
+		return -1;
+	}
+
+	addAtStart(last);
+	return 0;
+}
+
+//e) briše odreðeni element iz liste
+int deleteElement(position head, char elem[]) {
+	position curr = NULL, prev = NULL;
+	prev = FindPrev;
+
+	if (prev == NULL) {
+		printf("Takav element ne postoji!");
+		return -1;
+	}
+
+	curr = prev->next;
+	prev->next = curr->next;
+	free(curr);
+
+	return 0;
+
+
+	
+}
+
+
+
+
+void main() {
+
+	position Head = (position)malloc(sizeof(list));
+	Head->next = NULL;
+}
